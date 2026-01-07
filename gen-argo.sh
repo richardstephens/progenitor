2135985b293c9f7af8992784b309f1b486a2ca23
Richard Stephens <rjst01@gmail.com>
2026-01-07 18:24:27 +0100

    wip2

--- gen-argo.sh
+++ gen-argo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-RUST_BACKTRACE=1 cargo run -- progenitor --input argo-as-openapi3.json --output argocd-lib --name argocd-lib --version 0.1.0 
+RUST_BACKTRACE=1 cargo run -- progenitor \
+    --input argo-as-openapi3.json \
+    --output argocd-lib \
+    --name argocd-api-client \
+    --version 0.1.0 \
+    --interface builder
+
 
 
